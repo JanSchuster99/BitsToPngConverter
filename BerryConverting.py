40649288460bcab9c5301554e199325d6465616a
J.Schuster <2123206@stud.hs-mannheim.de>
2023-04-03 19:43:49 +0200

    New exe file und Bin support

--- BerryConverting.py
+++ BerryConverting.py
@@ -1,3 +1,4 @@
+import os
 import math
 from PIL import Image
 import tkinter as tk
@@ -6,7 +7,7 @@ from tkinter import messagebox
 import time
 # Es öffnet sich ein kleines Fenster mit einem Button "Generate Image"
 # Nach Druck auf den Button öffnet sich der Explorer 
-# Wähle eine TXT  DATEI welche NUR EINSEN UND NULLEN beinhaltet (KEINE LEERZEICHEN ODER UMBRÜCHE)
+# Wähle eine TXT DATEI welche NUR EINSEN UND NULLEN beinhaltet (KEINE LEERZEICHEN ODER UMBRÜCHE) oder einfach eine bin/Binary file(fck team GMTROM)
 # Wenn alles gucci läuft wird ein PNG mit dem aktuellen Timestamp als namen erzeugt
 # Die Größe des Bilds wird definiert durch die Anzahl der Chars im txt file. Es nimmt die sqrt der nächst größten Quadratzahl als länge und Breite
 # Bsp: file hat 5 chars => nächste größte Quadratzahl = 9 => 3x3 pixel (bitte nutzt das programm nicht nur mit ner sample size von 5 bits)
@@ -16,27 +17,30 @@ import time
 # Programm wurde 100% von mir geschrieben und komplett ohne hilfe von CHAT GPT ;) shit ist auch überhaupt nicht optimized oder gut geschrieben aber reicht lmao
 # Falls es irgendwelche Probleme gibt. fragt CHAT GPT oder schreibts einfach selbst um. CHAT GPT ist noch kostenlos o<()8°)
 # Hier sind 100% typos und Rechtschreibfehler, idc ty
-
-
-
 # Define a function to handle button click event
 def generate_image():
     try:
         # Get the path to the input file from file dialog
         input_file = filedialog.askopenfilename(title="Select input file")
 
-        # Open the text file and read all the bits
-        with open(input_file, 'r') as f:
+        # Determine the mode based on file extension
+        mode = 'r' if os.path.splitext(input_file)[1] != '.bin' else 'rb'
+
+        # Open the file and read all the bits
+        with open(input_file, mode) as f:
             bits = f.read()
+
+        # Convert the string of bits into a list of integers
+        if mode == 'r':
+            bits = [int(bit) for bit in bits.strip()]
+        else:
+            bits = [int(b) for byte in bits for b in '{:08b}'.format(byte)]
 
         # Calculate the dimensions of the image based on the square root of the number of bits
         dim = int(math.ceil(math.sqrt(len(bits))))
 
         # Create a dim x dim image with white background
         img = Image.new('1', (dim, dim), color=1)
-
-        # Convert the string of bits into a list of integers
-        bits = [int(bit) for bit in bits]
 
         # Iterate over the list of bits and set the corresponding pixel in the image
         for i, bit in enumerate(bits):
@@ -56,6 +60,7 @@ def generate_image():
     except Exception as e:
         # Show an error message if something goes wrong
         messagebox.showerror("Error", f"Failed to generate image: {str(e)}")
+
 
 # Create a GUI window
 root = tk.Tk()
---
+++ README.txt
@@ -0,0 +1,69 @@
+txt/bin file to png converter
+
+Für die Leute welche die EXE nutzen wollen. Die EXE braucht keine Python installation oder installation irgendwelcher libraries JEDOCH
+wird Windows und euer Browser euch sagen das es ein Virus ist deswegen nutzt einfach die Python datei in ner IDE LMAO 
+um die EXE herunterzuladen clickt auf BerryConverting.exe im Repo und dann auf Runterladen
+Das Programm öffnet ein fenster => auf "Generate Image" Clicken => im explorer das gewünschte file auswählen(NUR BIN ODER TXT)
+=> Bild wird im selben Pfad gespeichert wie exe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fck team GMTROM
